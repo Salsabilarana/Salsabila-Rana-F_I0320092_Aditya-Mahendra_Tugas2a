01d9ff2613f5bf65d4c818ae62a3879dc5a184f2
Salsabila Rana <slsblarana12@gmail.com>
2021-03-11 06:24:22 -0800

    Tugas 2

---
+++ I0320092_exercise2.4.py
@@ -0,0 +1,13 @@
+#menampilkan informasi program
+
+print("konversi suhu (farenheit ke celcius)")
+
+#input Suhu dalam farenheit
+F = float(input("masukkan suhu(farenheit):"))
+
+#melakukan konversi suhu ke celcius
+C = 5 * (F-32) / 9
+
+#menampilkan hasil konversi kelayar
+print("farenheit: ", F)
+print("Celcius :", C)
